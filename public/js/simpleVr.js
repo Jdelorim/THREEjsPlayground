2527cdf93cda9e5df44473d7b9c5ad9c04e033ff
Joshua deLorimier <TRASHTRASH_HQ@TRASHTRASHPC.localdomain>
2019-12-17 15:40:53 -0500

    tr

--- public/js/simpleVr.js
+++ public/js/simpleVr.js
@@ -30,8 +30,8 @@ const main = () => {
        shininess: 20, //default is 30
     });
     // const cube = new THREE.Mesh(jbox,jmat);
-    const spread = 60;
-    const count = 100;
+    const spread = 200;
+    const count = 1000;
     class MyCube {
         constructor(spread) {
             this.c =  new THREE.Mesh((new THREE.BoxBufferGeometry(4,4,4,1,1,1)),jmat);
@@ -48,21 +48,27 @@ const main = () => {
         scene.add(cubeHolder[i].c);
     }
     const light1 = new THREE.DirectionalLight('rgb(255,255,255)',1,50);
-    const light2 = new THREE.PointLight('rgb(255,100,255)',1,50);
+    const light2 = new THREE.PointLight('rgb(255,100,255)',1,100);
     scene.add(light1);
     scene.add(light2);
     
     light1.position.set(-4,0,10);
     light2.position.set(4,1.6,3);
     camera.position.set(0,1.6,3);
-  
 
+    const map =(value,  min1,  max1,  min2,  max2)=> {
+        return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+    }
+let speed = 0;
 const animate = () => { 
    renderer.setAnimationLoop( function () {
+      speed= speed+1;
        for(let i =0;i<count;i++) {
         cubeHolder[i].c.rotation.x += (i*0.04)/500;
         cubeHolder[i].c.rotation.y += (i*.05/2)/500;
        }
+       light2.intensity = map(Math.sin(speed),-1,1,0,2);
+    //    console.log(light2.intensity);
         
         renderer.render( scene, camera );
     } );
