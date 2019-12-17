0999cf179eab45f2238e3bb1d84391fe6b838298
Joshua deLorimier <TRASHTRASH_HQ@TRASHTRASHPC.localdomain>
2019-12-17 15:12:39 -0500

    vr working

--- public/js/simpleVr.js
+++ public/js/simpleVr.js
@@ -4,42 +4,55 @@
 const main = () => {
     const scene = new THREE.Scene();
     let camera  = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
-  
+    const vr = false;
     const renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     
-    //document.body.appendChild( renderer.domElement ); 
-    document.body.appendChild( VRButton.createButton( renderer ) );
-    renderer.vr.enabled = true;
+    document.body.appendChild( renderer.domElement ); 
+    if(vr) {
+    // document.body.appendChild( VRButton.createButton( renderer ) );
+    // renderer.vr.enabled = true;
+    }
 
-    // const onWindowResize = () => {
-    //     renderer.setSize( window.innerWidth, window.innerHeight );
-    //     camera.aspect = window.innerWidth / window.innerHeight;
-    //     camera.updateProjectionMatrix();
-    // }
-    // window.addEventListener( 'resize', onWindowResize, false );
+
 
     const jbox = new THREE.BoxBufferGeometry(1.5,1.5,1.5);
     const jmat = new THREE.MeshPhongMaterial({color: 'rgb(255,255,255)'});
-    const cube = new THREE.Mesh(jbox,jmat);
-    var light3 = new THREE.DirectionalLight('rgb(255,200,255)',1,50);
-    scene.add(light3);
-    scene.add(cube);
-    light3.position.set(-4,0,10);
+    // const cube = new THREE.Mesh(jbox,jmat);
+    const spread = 100;
+    class MyCube {
+        constructor(spread) {
+            this.c =  new THREE.Mesh((new THREE.BoxBufferGeometry(4,4,4,1,1,1)),jmat);
+            this.c.position.x = ((Math.random()-0.5)*spread);
+            this.c.position.y = ((Math.random()-0.5)*spread);
+            this.c.position.z = ((Math.random()-0.5)*spread);
+        }
+    
+    }
+   
+    const cubeHolder = [];
+    for(let i =0;i<100;i++) {
+        cubeHolder.push(new MyCube(spread));
+        scene.add(cubeHolder[i].c);
+    }
+    const light1 = new THREE.DirectionalLight('rgb(255,255,255)',1,50);
+    const light2 = new THREE.PointLight('rgb(255,100,255)',1,50);
+    scene.add(light1);
+    scene.add(light2);
+    
+    light1.position.set(-4,0,10);
+    light2.position.set(4,1.6,3);
     camera.position.set(0,1.6,3);
-    cube.rotation.x = 10;
-    cube.rotation.y = 5;
-    cube.position.z = -5;
+  
 
 const animate = () => { 
-   
-    // renderer.render(scene, camera);
-    //requestAnimationFrame(animate);
-    renderer.setAnimationLoop( function () {
-        cube.rotation.x += 0.04;
-        cube.rotation.y += .05/2;
+   renderer.setAnimationLoop( function () {
+       for(let i =0;i<100;i++) {
+        cubeHolder[i].c.rotation.x += 0.04;
+        cubeHolder[i].c.rotation.y += .05/2;
+       }
+        
         renderer.render( scene, camera );
-    
     } );
 }
 animate();
