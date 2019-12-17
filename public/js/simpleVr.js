5af248e4673c2e2209781f7f4d72c614e4d7a26d
Joshua deLorimier <TRASHTRASH_HQ@TRASHTRASHPC.localdomain>
2019-12-17 15:31:17 -0500

    vrtest4

--- public/js/simpleVr.js
+++ public/js/simpleVr.js
@@ -8,10 +8,12 @@ const main = () => {
     const renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     
-    document.body.appendChild( renderer.domElement ); 
+    
     if(vr) {
     document.body.appendChild( VRButton.createButton( renderer ) );
     renderer.vr.enabled = true;
+    } else {
+    document.body.appendChild( renderer.domElement ); 
     }
 
 
@@ -20,15 +22,16 @@ const main = () => {
     const jmat = new THREE.MeshPhongMaterial({
         map: new THREE.TextureLoader().load('textures/metalTex_COLOR.png'), 
         side: THREE.FrontSide,
-       normalMap: new THREE.TextureLoader().load('textures/metalTex_NRM.png'),
-       bumpMap:  new THREE.TextureLoader().load('textures/metalTex_DISP.png'),
-       bumpScale: 0.8,
-       aoMap: new THREE.TextureLoader().load('textures/metalTex_OCC.png'),
-       aoMapIntensity: 8,
+    //    normalMap: new THREE.TextureLoader().load('textures/metalTex_NRM.png'),
+    //    bumpMap:  new THREE.TextureLoader().load('textures/metalTex_DISP.png'),
+    //    bumpScale: 0.8,
+    //    aoMap: new THREE.TextureLoader().load('textures/metalTex_OCC.png'),
+    //    aoMapIntensity: 8,
        shininess: 20, //default is 30
     });
     // const cube = new THREE.Mesh(jbox,jmat);
-    const spread = 100;
+    const spread = 60;
+    const count = 100;
     class MyCube {
         constructor(spread) {
             this.c =  new THREE.Mesh((new THREE.BoxBufferGeometry(4,4,4,1,1,1)),jmat);
@@ -40,7 +43,7 @@ const main = () => {
     }
    
     const cubeHolder = [];
-    for(let i =0;i<1000;i++) {
+    for(let i =0;i<count;i++) {
         cubeHolder.push(new MyCube(spread));
         scene.add(cubeHolder[i].c);
     }
@@ -56,7 +59,7 @@ const main = () => {
 
 const animate = () => { 
    renderer.setAnimationLoop( function () {
-       for(let i =0;i<1000;i++) {
+       for(let i =0;i<count;i++) {
         cubeHolder[i].c.rotation.x += (i*0.04)/500;
         cubeHolder[i].c.rotation.y += (i*.05/2)/500;
        }
