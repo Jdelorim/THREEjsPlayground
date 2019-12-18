cfb99bcfce6bf5059e53fa37e575d683ae271dfa
Joshua deLorimier <TRASHTRASH_HQ@TRASHTRASHPC.localdomain>
2019-12-18 17:43:01 -0500

    e

--- public/js/simpleVr.js
+++ public/js/simpleVr.js
@@ -7,12 +7,12 @@ const main = () => {
     const scene = new THREE.Scene();
     const simplex = new SimplexNoise();
     let camera  = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
-    
+    let vr = true;
     const renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     
     
-    if(renderer.vr.isPresenting()) {
+    if(vr === true) {
     document.body.appendChild( VRButton.createButton( renderer ) );
     renderer.vr.enabled = true;
     } else {
